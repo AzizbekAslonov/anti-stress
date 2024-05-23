8769a99e149a188b049f22d51eee77d0f465d434
AzizbekAslonov <aslonovazizbek318@gmail.com>
2024-05-24 03:01:46 +0500

    big changes 2

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ document.addEventListener("mousemove", (e) => {
   mouse.x = e.x;
   mouse.y = e.y;
 
-  for (let i = 0; i < 3; i++) {
+  for (let i = 0; i < 1; i++) {
     particlesArr.push(new Particle());
   }
 });
@@ -25,16 +25,25 @@ class Particle {
   constructor() {
     this.x = mouse.x;
     this.y = mouse.y;
-    this.size = Math.random() * 10 + 1;
-    this.speedX = Math.random() * 10 - 5;
-    this.speedY = Math.random() * 10 - 5;
+    this.size = Math.random() * 5 + 1;
+    this.speedX = Math.random() * 6 - 3;
+    this.speedY = Math.random() * 6 - 3;
     this.color = `hsl(${hue}, 100%, 50%)`;
   }
 
   update() {
     this.x += this.speedX;
     this.y += this.speedY;
-    if (this.size > 0.2) this.size -= 0.1;
+
+    if (this.x + 7 >= innerWidth || this.x - 7 <= 0) {
+      this.speedX = -this.speedX;
+    }
+
+    if (this.y + 7 >= innerHeight || this.y - 7 <= 0) {
+      this.speedY = -this.speedY;
+    }
+
+    if (this.size > 1) this.size -= 0.005;
   }
 
   draw() {
@@ -62,13 +71,14 @@ function handleParticles() {
       const dis = Math.sqrt(dx * dx + dy * dy);
       if (dis < 80) {
         ctx.beginPath();
+        ctx.lineWidth = 1;
         ctx.strokeStyle = particlesArr[i].color;
         ctx.moveTo(particlesArr[i].x, particlesArr[i].y);
         ctx.lineTo(particlesArr[j].x, particlesArr[j].y);
         ctx.stroke();
       }
     }
-    if (elem.size <= 0.2) {
+    if (elem.size <= 1) {
       particlesArr.splice(i, 1);
       i--;
     }
@@ -78,8 +88,7 @@ function handleParticles() {
 handleParticles();
 
 function animate() {
-  // ctx.clearRect(0, 0, canvas.width, canvas.height)
-  ctx.fillStyle = "rgba(0,0,0,0.1)";
+  ctx.fillStyle = "rgba(0,0,0,0.2)";
   ctx.fillRect(0, 0, canvas.width, canvas.height);
   handleParticles();
   hue += 0.5;
